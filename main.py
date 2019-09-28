cfc31637bc30e47c98a027a9f8f8ad3bc93fb13f
AlexTrubkina <55000120+AlexTrubkina@users.noreply.github.com>
2019-09-28 12:30:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ def main():
     a = float(input("Введите а: "))
     b = float(input("Введите b: "))
     c = float(input("Введите с: "))
-    res = grn(a, b, c)
-    print(res)
+    if a + b < c or a + c < b or b + c < a and a > 0 and b > 0 and c > 0:
+        res = grn(a, b, c)
+        print(res)
+    else: 
+        print("Треугольника не существует")
 
 
 def grn(a, b, c):
+    """
+    Расчет площади треугольника по формуле Герона
+    """
     ppr = (a+b+c) / 2
     pl = sqrt(ppr * (ppr-a) * (ppr-b) * (ppr-c))
     return pl
