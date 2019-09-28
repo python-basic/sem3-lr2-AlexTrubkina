81afd247369d26ea2e23851ff3df7152e2d236a1
AlexTrubkina <55000120+AlexTrubkina@users.noreply.github.com>
2019-09-28 12:09:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import math
+def main():
+    a = float(input("Введите а: "))
+    b = float(input("Введите b: "))
+    c = float(input("Введите с: "))
+    res = grn(a, b, c)
+    print(res)
 
+
+def grn(a, b, c):
+    ppr = (a+b+c) / 2
+    pl = sqrt(ppr * (ppr-a) * (ppr-b) * (ppr-c))
+    return pl
