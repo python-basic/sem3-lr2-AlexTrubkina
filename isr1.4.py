522a6cbeab82c93afd2742106b670d8b6398ba42
AlexTrubkina <55000120+AlexTrubkina@users.noreply.github.com>
2021-06-29 01:14:50 +0300

    Create isr1.4.py

---
+++ isr1.4.py
@@ -0,0 +1,11 @@
+def operations(a, b):
+    print("Сложение: ")
+    print(a, " + ", b, " = ", a + b)
+    print("Вычитание")
+    print(a, " - ", b, " = ", a - b)
+    print("Умножение")
+    print(a, " * ", b, " = ", a * b)
+    print("Деление")
+    print(a, " / ", b, " = ", a / b)
+
+operations(1, 2)
